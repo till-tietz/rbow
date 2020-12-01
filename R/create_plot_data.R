f3f261ee32fd77aa30d7469b017de9c623ae1793
till-tietz <ttietz2014@gmail.com>
2020-12-01 11:05:35 +0100

    added ability to process multiple texts and add meta data to create_plot_data function.

--- R/create_plot_data.R
+++ R/create_plot_data.R
@@ -1,27 +1,48 @@
 #'re-format output of bow_ci function to plottable format
 #'
 #'@param bstrap_output output of bow_ci function
-#'@return data frame of plot data
+#'@param meta_data a data frame of meta data for each analyzed text i.e year, author etc.
+#'@param combine if TRUE combines plotting data for each text into one data frame; if false returns a data frame for each text; default FALSE
+#'@return data frame or list of data frames
 #'@export
 
-create_plot_data <- function(bstrap_output){
-  n_descriptors <- unique(bstrap_output$descriptor)
-  n_phenomena <- unique(bstrap_output$phenomenon)
+create_plot_data <- function(bstrap_output, meta_data = NULL, combine = FALSE){
+  each_text <- function(x){
+    bstrap_output_i <- bstrap_output[[x]]
+    n_descriptors <- unique(bstrap_output_i[,"descriptor"])
+    n_phenomena <- unique(bstrap_output_i[,"phenomenon"])
 
-  if(length(n_phenomena) %% 2 == 0){
-    step_size <- (length(n_phenomena)/2)/10
-    steps <- seq(from = 1 - step_size, to = 1 + step_size, by = 0.1)
-    steps <- steps[!steps %in% 1]
-  } else {
-    step_size <- ((length(n_phenomena)-1)/2)/10
-    steps <- seq(from = 1 - step_size, to = 1 + step_size, by = 0.1)
+    if(length(n_phenomena) %% 2 == 0){
+      step_size <- (length(n_phenomena)/2)/10
+      steps <- seq(from = 1 - step_size, to = 1 + step_size, by = 0.1)
+      steps <- steps[!steps %in% 1]
+    } else {
+      step_size <- ((length(n_phenomena)-1)/2)/10
+      steps <- seq(from = 1 - step_size, to = 1 + step_size, by = 0.1)
+    }
+
+    steps <- rep(steps, each = length(n_descriptors))
+    pseudo <- rep(c(0:(length(n_descriptors)-1)), length(n_phenomena))
+    pseudo_y <- steps + pseudo
+
+    out <- bstrap_output_i
+    out[,"pseudo_y"] <- pseudo_y
+
+    if(!is.null(meta_data)){
+      meta_df <- as.data.frame(sapply(meta_data[x,], function(x) rep(x, nrow(out))))
+      out <- cbind(out, meta_df)
+    } else {
+      out <- out
+    }
+    return(out)
   }
 
-  steps <- rep(steps, each = length(n_descriptors))
-  pseudo <- rep(c(0:(length(n_descriptors)-1)), length(n_phenomena))
-  pseudo_y <- steps + pseudo
+  plot_data <- purrr::map(1:length(bstrap_output), ~each_text(.x))
 
-  out <- bstrap_output
-  out$pseudo_y <- pseudo_y
-  return(out)
+  if(combine == TRUE){
+    plot_data <- do.call(rbind, plot_data)
+  } else {
+    plot_data <- plot_data
+  }
+  return(plot_data)
 }
