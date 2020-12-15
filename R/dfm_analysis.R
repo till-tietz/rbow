4cc24844cbe3291c6c80115d54a1087893fd65c4
till-tietz <ttietz2014@gmail.com>
2020-12-15 15:41:53 +0100

    added tf-idf computation functionality

--- R/dfm_analysis.R
+++ R/dfm_analysis.R
@@ -6,76 +6,114 @@
 #' @param n_terms number of terms displayed in dfm
 #' @param filter_ps if TRUE enables filtering of results by part of speech (i.e only adjectives and adverbs)
 #' @param ps character vector of parts of speech to filter. see selection with unique(tidytext::parts_of_speech[,"pos"])
+#' @param tf_idf if TRUE function computes tf-idf metric instead of raw counts
 #' @return list of dfms (one dfm per text in corpus)
 #' @export
 
-dfm_analysis <- function(corpus, phenomenon, window = 10, n_terms = 10, filter_ps = FALSE, ps = NULL) {
-  phenomenon_grep <-
-    lapply(phenomenon, function(x)
-      paste(x, sep = "", collapse = "|"))
+dfm_analysis <-
+  function(corpus,
+           phenomenon,
+           window = 10,
+           n_terms = 10,
+           tf_idf = FALSE ,
+           filter_ps = FALSE,
+           ps = NULL) {
+    phenomenon_grep <-
+      lapply(phenomenon, function(x)
+        paste(x, sep = "", collapse = "|"))
 
-  #set up loop over each text
-  each_text <- function(x) {
-    corpus_i <- corpus[[x]]
+    #set up loop over each text
+    each_text <- function(x) {
+      corpus_i <- corpus[[x]]
 
-    #set up loop over each phenomenon
-    each_phenomenon <- function(x) {
-      phenomenon_i <- phenomenon_grep[[x]]
+      #set up loop over each phenomenon
+      each_phenomenon <- function(x) {
+        phenomenon_i <- phenomenon_grep[[x]]
 
-      #get position of occurences of phenomenon terms in text
-      #(i.e "bla bla bla spy bla bla inspect" = c(4,7))
-      positions <- grep(phenomenon_i, corpus_i)
+        #get position of occurences of phenomenon terms in text
+        #(i.e "bla bla bla spy bla bla inspect" = c(4,7))
+        positions <- grep(phenomenon_i, corpus_i)
 
-      #set up control flow i.e only execute rest of analysis if phenomenon terms occur in text
-      if (length(positions) > 0) {
-        #define window around phenomenon occurences
-        #define lower bound of window
-        min_position <- positions - window
-        #limit lower bound to 0
-        #(otherwise a phenomenon at text position 2 with window = 20 would have a lower window bound
-        # -18 thus causing an error in subsetting the text vector )
-        min_position[which(min_position <= 0)] <- 1
-        #define upper bound of window
-        max_position <- positions + window
-        #limit upper bound to length of text (same reason as above)
-        max_position[which(max_position > length(corpus_i))] <-
-          length(corpus_i)
-        #get text snippets around phenomena
-        analysis_text <-
-          unlist(
+        #set up control flow i.e only execute rest of analysis if phenomenon terms occur in text
+        if (length(positions) > 0) {
+          #define window around phenomenon occurences
+          #define lower bound of window
+          min_position <- positions - window
+          #limit lower bound to 0
+          #(otherwise a phenomenon at text position 2 with window = 20 would have a lower window bound
+          # -18 thus causing an error in subsetting the text vector )
+          min_position[which(min_position <= 0)] <- 1
+          #define upper bound of window
+          max_position <- positions + window
+          #limit upper bound to length of text (same reason as above)
+          max_position[which(max_position > length(corpus_i))] <-
+            length(corpus_i)
+          #get text snippets around phenomena
+          analysis_text <-
             text_window(
               text = corpus_i,
               start = min_position,
               end = max_position,
-              position = positions
+              position = positions,
+              out_of_window = TRUE
             )
-          )
 
-        #get the number of occurences for each term
-        dfm <-
-          as.data.frame(sort(table(analysis_text), decreasing = TRUE)[c(1:n_terms)])
 
-        if(filter_ps == TRUE){
-          terms <- merge(dfm, tidytext::parts_of_speech, by.x = "analysis_text", by.y = "word")
-          terms <- unique(terms[which(terms[,"pos"] %in% ps),1])
+          if (tf_idf == FALSE) {
+            #get the number of occurences for each term
+            dfm <-
+              as.data.frame(sort(table(unlist(
+                analysis_text[[1]]
+              )), decreasing = TRUE)[c(1:n_terms)])
+          } else {
+            tf <-
+              as.data.frame(table(unlist(analysis_text[[1]])))
+            tf[, 2] <-
+              tf[, 2] / length(unlist(analysis_text[[1]]))
 
-          dfm <- dfm[which(dfm[,"analysis_text"] %in% terms),]
+            idf <-
+              as.data.frame("word" = unique(unlist(analysis_text[[1]])),
+                            "idf" = log(length(analysis_text) / (
+                              as.numeric(unique(unlist(
+                                analysis_text[[1]]
+                              )) %in% analysis_text[[2]])
+                            )))
+
+            dfm <-
+              merge(tf,
+                    idf,
+                    by.x = colnames(tf)[1],
+                    by.y = colnames(idf)[1])
+            dfm[, "tf-idf"] <- dfm[, 2] * dfm[, 3]
+            dfm <- dfm[, c(1, 4)]
+            dfm <- dplyr::arrange(dfm, desc(colnames(dfm)[2]))
+          }
+
+          if (filter_ps == TRUE) {
+            terms <-
+              merge(dfm,
+                    tidytext::parts_of_speech,
+                    by.x = "analysis_text",
+                    by.y = "word")
+            terms <- unique(terms[which(terms[, "pos"] %in% ps), 1])
+
+            dfm <- dfm[which(dfm[, "analysis_text"] %in% terms), ]
+          } else {
+            dfm <- dfm
+          }
+
         } else {
-          dfm <- dfm
+          dfm <- NA
         }
-
-      } else {
-        dfm <- NA
+        return(dfm)
       }
-      return(dfm)
+      #execute loop over phenomena
+      each_phenomenon_out <-
+        purrr::map(1:length(phenomenon), ~ each_phenomenon(.x))
+      names(each_phenomenon_out) <- names(phenomenon)
+      return(each_phenomenon_out)
     }
-    #execute loop over phenomena
-    each_phenomenon_out <-
-      purrr::map(1:length(phenomenon), ~ each_phenomenon(.x))
-    names(each_phenomenon_out) <- names(phenomenon)
-    return(each_phenomenon_out)
+    #execute loop over texts
+    each_text_out <- purrr::map(1:length(corpus), ~ each_text(.x))
+    return(each_text_out)
   }
-  #execute loop over texts
-  each_text_out <- purrr::map(1:length(corpus), ~ each_text(.x))
-  return(each_text_out)
-}
