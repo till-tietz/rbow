f7db8b1020147869d4622022b06f8d88816a1a93
till-tietz <ttietz2014@gmail.com>
2020-12-15 15:42:30 +0100

    added ability to return non window text

--- R/text_window.R
+++ R/text_window.R
@@ -4,15 +4,29 @@
 #'@param start start position
 #'@param end end position
 #'@param position vector of word positions around which a window is constructed
+#'@param out_of_window if TRUE function returns set of words within and outside of window
 #'@export
 
-text_window <- function(text, start, end, position) {
-  loop <- function(x) {
-    position_vec <- c(start[x]:end[x])
-    position_vec <- position_vec[!position_vec %in% position[x]]
-    text_window_i <- text[position_vec]
-    return(text_window_i)
+text_window <- function(text, start, end, position, out_of_window = FALSE) {
+    loop_window <- function(x) {
+      position_vec <- c(start[x]:end[x])
+      position_vec <- position_vec[!position_vec %in% position[x]]
+      text_window_i <- text[position_vec]
+      return(text_window_i)
+    }
+    loop_w_out <- purrr::map(1:length(start), ~ loop_window(.x))
+
+    if (out_of_window == TRUE) {
+      loop_out_of_window <- function(x) {
+        position_vec <- c(start[x]:end[x])
+        return(position_vec)
+      }
+      loop_ofw_out <- purrr::map(1:length(start), ~ loop_out_of_window(.x))
+      ofw_index <- unique(unlist(loop_ofw_out))
+      ofw_text <- text[-ofw_index]
+      out <- list(w_text = loop_w_out, ofw_text = ofw_text)
+    } else {
+      out <- loop_w_out
+    }
+    return(out)
   }
-  out <- purrr::map(1:length(start), ~ loop(.x))
-  return(out)
-}
