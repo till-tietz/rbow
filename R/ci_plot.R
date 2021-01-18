127c510ded311500e6353dc8fd5ab9fcbba6ae00
till-tietz <ttietz2014@gmail.com>
2021-01-18 12:01:47 +0100

    can now choose between shape or colour scales for geom_point

--- R/ci_plot.R
+++ R/ci_plot.R
@@ -1,15 +1,15 @@
 #'plot results of bow_analysis with bow_ci confidence intervals
 #'
 #'@param plot_data output of create_plot_data function
+#'@param scale character vector specifying whether phenomena should be plotted with different colours or shapes. options are "shape" or "colour".
 #'@return a ggplot object
 #'@export
 
 
 
-ci_plot <- function(plot_data){
+ci_plot <- function(plot_data, scale = "shape"){
 
   plot <- ggplot2::ggplot(plot_data)+
-    ggplot2::geom_point(ggplot2::aes(x = observed_mean, y = pseudo_y, shape = factor(phenomenon)), size = 1.5)+
     ggplot2::geom_segment(ggplot2::aes(x = plot_data[,2], y = plot_data[,6], xend = plot_data[,3], yend = plot_data[,6]), size = 0.2) +
     ggplot2::xlab("mean occurence") +
     ggplot2::theme_bw() +
@@ -18,6 +18,16 @@ ci_plot <- function(plot_data){
                        labels = unique(plot_data[,"descriptor"]))+
     ggplot2::theme(axis.title.y = ggplot2::element_blank(),
           legend.title = ggplot2::element_blank())
+
+  if(scale == "shape"){
+    plot <- plot+
+      ggplot2::geom_point(ggplot2::aes(x = observed_mean, y = pseudo_y, shape = as.factor(phenomenon)), size = 1.5)
+  }
+
+  if(scale == "colour"){
+    plot <- plot+
+      ggplot2::geom_point(ggplot2::aes(x = observed_mean, y = pseudo_y, colour = as.factor(phenomenon)), size = 1.5)
+  }
 
   return(plot)
 }
