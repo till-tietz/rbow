a423e5101bc5db0d288bcd1b8b1e3e6c5240f057
till-tietz <ttietz2014@gmail.com>
2021-03-11 20:29:19 +0100

    fixed string end issue

--- R/grep_construct.R
+++ R/grep_construct.R
@@ -13,9 +13,14 @@ grep_construct <- function(text_input, collapse = FALSE){
     loop <- function(x){
       text_i <- text_input[[x]]
 
-      text_i[-grep("*", text_i, fixed = TRUE)] <- paste(text_i[-grep("*", text_i, fixed = TRUE)], "\\b")
-      text_i <- gsub("\\*", "", text_i)
-      text_i <- gsub(" ", "", text_i)
+      if(length(grep("*", text_i, fixed = TRUE)) > 0){
+        text_i[-grep("*", text_i, fixed = TRUE)] <- paste(text_i[-grep("*", text_i, fixed = TRUE)], "\\b")
+        text_i <- gsub("\\*", "", text_i)
+        text_i <- gsub(" ", "", text_i)
+      } else {
+        text_i <- paste(text_i,"\\b", sep = "")
+      }
+
       if(collapse == TRUE){
         text_i <- paste(text_i, sep = "", collapse = "|\\b")
       }
@@ -25,9 +30,15 @@ grep_construct <- function(text_input, collapse = FALSE){
     out <- purrr::map(1:length(text_input), ~loop(.x))
   } else {
     text_i <- text_input
-    text_i[-grep("*", text_i, fixed = TRUE)] <- paste(text_i[-grep("*", text_i, fixed = TRUE)], "\\b")
-    text_i <- gsub("\\*", "", text_i)
-    text_i <- gsub(" ", "", text_i)
+
+    if(length(grep("*", text_i, fixed = TRUE)) > 0){
+      text_i[-grep("*", text_i, fixed = TRUE)] <- paste(text_i[-grep("*", text_i, fixed = TRUE)], "\\b")
+      text_i <- gsub("\\*", "", text_i)
+      text_i <- gsub(" ", "", text_i)
+    } else {
+      text_i <- paste(text_i,"\\b", sep = "")
+    }
+
     if(collapse == TRUE){
       text_i <- paste(text_i, sep = "", collapse = "|\\b")
     }
@@ -36,5 +47,3 @@ grep_construct <- function(text_input, collapse = FALSE){
   }
   return(out)
 }
-
-
