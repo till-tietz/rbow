8613d337f2c2537eaa7df80baf3a32761bb87e3d
till-tietz <ttietz2014@gmail.com>
2021-03-16 16:24:35 +0100

    added stopword support for multiple languages

--- R/clean_text.R
+++ R/clean_text.R
@@ -3,28 +3,49 @@
 #'@name clean_text
 #'
 #'@param texts list of character vectors
-#'@param lexicon stop word lexicon to use
+#'@param rm_stopwords if TRUE clean_text removes stopwords (default FALSE)
+#'@param stopwords_language stopword dictionary language ISO 639-1 code as character vector
 #'@return list of cleaned character vectors
 #'@export
 
 
-clean_text <- function(texts, lexicon = "SMART"){
-  stop.words <- tidytext::stop_words
-  stop.words <- as.data.frame(stop.words[which(stop.words[,"lexicon"] == lexicon),])[,"word"]
+clean_text <- function(texts, rm_stopwords = FALSE, stopwords_language = NULL){
+
+  iso_codes <- as.character(ISOcodes::ISO_639_2[["Alpha_2"]])
+  iso_codes <- iso_codes[!is.na(iso_codes)]
+
+  if(rm_stopwords == TRUE){
+
+    if(is.null(stopwords_language)){
+      stop("no stopwords_language specified")
+    }
+
+    if(!stopwords_language %in% iso_codes){
+      stop("did not specify valid ISO 639-1 code for stopwords_language")
+    }
+
+    stop_words <- stopwords::stopwords(language = stopwords_language,
+                                       source = "snowball",
+                                       simplify = TRUE)
+  }
 
   each_text <- function(x){
     text_i <- as.character(texts[[x]])
-    clean_text <- gsub('[[:punct:] ]+',' ', text_i)
-    clean_text <- gsub('[[:digit:]]+', '', clean_text)
-    clean_text <- tolower(clean_text)
+    clean_text <- tolower(text_i)
+    clean_text <- gsub("[^a-zA-Z\\s]", " ", clean_text)
+    clean_text <- trimws(clean_text, which = "both")
+    clean_text <- gsub("\\s+", " ", clean_text)
     clean_text <- unlist(strsplit(clean_text, split=" "))
-    clean_text <- clean_text[-(which(clean_text %in% c("","\n","\f\n")))]
-    clean_text <- trimws(clean_text, which = c("both"))
 
-    clean_text <- clean_text[-(which(clean_text %in% stop.words))]
+    if(rm_stopwords == TRUE){
+      clean_text <- clean_text[!clean_text %in% stop_words]
+    }
+
     return(clean_text)
   }
   out <- furrr::future_map(1:length(texts), ~each_text(.x), .progress = TRUE)
   names(out) <- names(texts)
   return(out)
 }
+
+
